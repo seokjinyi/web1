c8bfcf4e6643657a7d4db378ba0f93d17d1584ca
seokjinyi <32754123+seokjinyi@users.noreply.github.com>
2018-12-03 01:34:45 +0900

    a add

---
+++ a.php
@@ -0,0 +1,46 @@
+<!-- 
+<html>
+<head>
+<script language ="JavaScript">
+
+function agree(obj){
+   if(obj.checked == true) {
+      a2.disabled = false;
+      }else {
+         a2.disabled = true;
+         }
+   }
+
+function sub() {
+   if(a1.checked == false) {
+       alert("check plz");
+      }
+
+
+   }
+
+
+</script>
+</head>
+<body>
+<center>
+<H2>Create account</H2>
+<textarea rows = 10 cols = 40>
+Welcom to kmove.com
+By using our Services, you agree to these conditions.
+blah blah ~
+</textarea>
+<br>
+<input type = 'checkbox' name = a1 value = a1 onClick = 'agree(this)'>I accept the terms of service <br>
+<br>
+Email <input type = 'text' name = a2 disabled>
+<br>
+<br>
+<input type = 'submit' value = 'Submit' onClick = 'sub()'>
+
+</center>
+</body>
+
+
+</html> -->
+
